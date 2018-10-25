0b727333618bddf1659b6678cdb69d39838fb404
ALBERTO GABRIEL LOPEZ <alblopez@MacBook-Pro-6.local>
2018-10-25 18:28:04 -0300

    terminado

---
+++ js/payment.js
@@ -0,0 +1,61 @@
+var preferenceId = qs("preference_id");
+$(document).ready(function () {
+    onDocumentLoad();
+    buildCheckoutForm()
+})
+
+function onDocumentLoad(){
+    getAmountFromPreferenceId(preferenceId);
+}
+
+function buildTokenizeForm(amount){
+    var s = document.createElement('script');
+    s.setAttribute('src', "https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js");
+    s.setAttribute('data-public-key', "TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd");
+    s.setAttribute('data-transaction-amount', amount);
+    document.getElementById("tokenizeForm").appendChild(s);
+
+    /*return '<script\n' +
+        '                    src="https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js"\n' +
+        '                    data-public-key="TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd"\n' +
+        '                    data-transaction-amount="' + amount + '">\n' +
+        '            </script>'*/
+}
+
+function buildCheckoutForm(){
+    var s = document.createElement('script');
+    s.setAttribute('src', "https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js");
+    s.setAttribute('data-preference-id', preferenceId);
+    document.getElementById("checkoutForm").appendChild(s);
+
+    /*return '<script id="process_preference"\n' +
+        '                    src="https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js"\n' +
+        '                    data-preference-id="' + preferenceId + '">\n' +
+        '            </script>'*/
+}
+
+function getAmountFromPreferenceId(preference_id) {
+    var url = "http://localhost:8089/getAmountFromPreferenceId"
+    var param = "?preference_id=" + preference_id;
+    $.ajax({
+        type: "GET",
+        url: url + param,
+        dataType: "text",
+        success: function (data, status, jqXHR) {
+            buildTokenizeForm(data);
+        }
+    });
+}
+
+function qs(search_for) {
+    var query = window.location.search.substring(1);
+    var parms = query.split('&');
+    for (var i = 0; i < parms.length; i++) {
+        var pos = parms[i].indexOf('=');
+        if (pos > 0 && search_for == parms[i].substring(0, pos)) {
+            return parms[i].substring(pos + 1);
+            ;
+        }
+    }
+    return "";
+}
---
+++ js/v1.js
@@ -0,0 +1,162 @@
+var token = "";
+var installments;
+var installmentsMessages;
+var issuerName;
+var public_key = "TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd";
+
+var currentTab = 0;
+
+var selectedInstallments;
+var amount;
+
+
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 0) {
+        $(".BUTTON_AGF").text("Tokenizar");
+        getIdTypes();
+    } else {
+        $(".BUTTON_AGF").text("CHECKOUT");
+    }
+    x[n].style.display = "block";
+}
+
+function getIdTypes() {
+    var url = "http://localhost:8089/getIdTypes"
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+            var selector = $("#id_selector");
+            var types = (JSON.parse(jqXHR.responseText));
+            $.each(types, function (id, type) {
+                selector.append($('<option value="' + id + '">' + type + '</option>'))
+            });
+        }
+    });
+}
+
+function hideTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "none";
+}
+
+function tokenize() {
+    if (token == "") {
+        var url = "https://api.mercadopago.com/v1/card_tokens"
+        var params = "?" +
+            "public_key=" + public_key;
+
+        var obj = {
+            card_number: $("#card_number :input").val(),
+            security_code: $("#cvv :input").val(),
+            expiration_month: $("#month").val(),
+            expiration_year: 20 + $("#year").val(),
+            cardholder: {
+                name:"RECH",
+                identification: {
+                    number: $("#idNumber :input").val(),
+                    type: $("#id_selector option:selected").val()
+                }
+            }
+        };
+
+        console.log(obj);
+
+        $.ajax({
+            type: "POST",
+            url: url + params,
+            data: JSON.stringify(obj),
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            dataType: "json",
+            success: function (data, status, jqXHR) {
+                token = JSON.parse(jqXHR.responseText).id;
+                getInstallments();
+            }
+        });
+
+    }
+}
+
+function getInstallments() {
+    alert("token when calling installments is = [" + token + "]");
+    amount = $("#amount :input").val();
+    var url = "http://localhost:8089/installments"
+    var params = "?" +
+        "card_token=" + token +
+        "&payment_amount=" + amount;
+
+    $.ajax({
+        type: "GET",
+        url: url + params,
+        contentType: "application/json; charset=utf-8",
+        crossDomain: true,
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+            installments = JSON.parse(jqXHR.responseText).installments;
+            installmentsMessages = JSON.parse(jqXHR.responseText).recommended_messages;
+            issuerName = JSON.parse(jqXHR.responseText).issuer_name;
+            updateInstallments();
+            showTab(currentTab);
+            hideTab(document.getElementsByClassName("tab").length - 1)
+        },
+        error: function (jqXHR, status) {
+            console.log(jqXHR);
+            alert('fail' + status.code);
+            return;
+        }
+    });
+}
+
+
+function next() {
+    var x = document.getElementsByClassName("tab")
+    showTab(x.length - 1);
+    if (currentTab === 0) {
+        $("#loading_text").text("POR FAVOR ESPERA...");
+        tokenize();
+    } else if (currentTab === 1) {
+        $("#loading_text").text("REALIZANDO PAGO, POR FAVOR ESPERA...");
+        var index = $("#intallments_selector option:selected").val();
+        selectedInstallments = installments[index];
+        makePayment();
+    }
+
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + 1;
+}
+
+function updateInstallments() {
+    var cuotas = $("#cuotas");
+    var selector = $("#intallments_selector");
+    $("#issuerName").append(issuerName);
+    $.each(installmentsMessages, function (index, msj) {
+        cuotas.append($('<p>' + msj + '</p>'));
+        selector.append($('<option value="' + index + '">' + msj + '</option>'))
+    });
+}
+
+function makePayment() {
+    var url = "http://localhost:8089/makePayment"
+    var params = "?" +
+        "installments=" + selectedInstallments +
+        "&amount=" + amount +
+        "&card_token=" + token;
+
+    $.ajax({
+        type: "POST",
+        url: url + params,
+        crossDomain: true,
+        dataType: "text",
+        success: function (data, status, jqXHR) {
+            hideTab(document.getElementsByClassName("tab").length - 1)
+            alert("Pago confirmado!!");
+        },
+        error: function (data, status, jqXHR) {
+            hideTab(document.getElementsByClassName("tab").length - 1)
+            alert("Pago rechazado :( \nStatus: " + status);
+        }
+    });
+}
